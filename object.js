fea3c7e7a4a62848ce0d2efc06864003407ab284
RasikaSonavane <rsika2014@gmail.com>
2021-09-20 16:27:46 +0530

    hi

---
+++ object.js
@@ -0,0 +1,11 @@
+var studentDetails=
+{
+Name: "Rasika", lastName: "Sonavane", age:27, married:false, 
+address:{city: "pen", Pincode: 402107}, 
+Mobile: 7045023783
+}
+
+console.log(studentDetails.Name);
+console.log(studentDetails.Age);
+console.log(studentDetails.address.Pincode);
+console.log(studentDetails['lastName'])
---
+++ typeOfDataType.js
@@ -0,0 +1,11 @@
+
+
+
+counter = false  // counter is now a boolean
+console.log(typeof(counter)) // "booleanva
+counter = 123  // counter is now a boolean
+console.log(typeof(counter)) // "booleanva
+counter = "false"  // counter is now a boolean
+console.log(typeof(counter)) // "booleanva
+
+
