378a66b97c584d0440a189e18208c9d005d9362b
RasikaSonavane <rsika2014@gmail.com>
2021-09-26 16:46:32 +0530

    string practice

--- string.js
+++ string.js
@@ -1,1 +1,63 @@
-var details={firstName:"Rasika", lastName:"Sonavane", mobileNum:8149291293, Address={houseNum:111, Country:"India"}, isGraduate=true, isMarried:false}
+    /*
+    var firstName="Siya";
+    var lastName="Dravid";
+    console.log(firstName + " "+ lastName)
+    //same can be done using string literals
+    console.log('My full name is ${firstName} ${lastName}')
+    // \n using for the new line
+
+    //length of the string 
+    var checkLength= firstName.length
+    console.log(checkLength)
+
+    var lastNameLength= lastName.length
+    console.log(lastNameLength)
+
+    //to print the specific character of the string
+    //by indexing its possible index always start with 0 count
+
+    console.log(firstName[2])
+    console.log(lastName[3])
+
+    //to find the last character of the string
+    //lengthof the string-1= last character index
+
+    console.log(firstName[checkLength-1])
+    console.log(lastName[lastNameLength-1])
+
+    var i=0;
+    for(i=0; i<firstName.length; i++)
+    {
+    console.log(firstName[i]);
+    }
+
+//to convert the number into string we use "tostring" function,
+//it will help to convert any dataType into string
+var number=80
+var convertIntoString= number.toString()
+console.log(convertIntoString)
+
+//vice a versa to convert string into number we use parsInt
+number=parseInt(convertIntoString)
+
+
+if('a'<'b'){console.log("a is less than b")}
+if('a'<'B'){console.log("a is less than B")}
+else{console.log("a is greater than b")}
+
+
+//to convert string to upparCase
+console.log(firstName.toUpperCase())
+console.log(lastName.toLowerCase())
+*/
+
+var studentDetails={
+    Address:{houseNum:104, society:"shree ganesh", city:"Panvel", country:"India", pinCode: 499855},
+   firstName: "Siya",
+   lastName: "Dravid",
+   Age: 23,
+   isGraduate:true,
+   isMarried:false,
+   mobileNumber: 7045023783
+}
+
